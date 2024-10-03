84f3e4d2351479a1463af7c2955e748355186c71
Sergey <chiki73rus@yandex.ru>
2024-10-03 19:02:11 +0400

    Создал экземпляры WeatherLocalDataSource и UseCases, передал UseCase'ов в WeatherScreen через конструктор

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/data/repositories/weather_local_data_source.dart';
+
+import 'package:weather_app/domian/usecases/load_weather_usecase.dart';
+import 'package:weather_app/domian/usecases/save_weather_usecase.dart';
 import 'package:weather_app/presentation/screens/weather_screen.dart';
 import 'package:weather_app/presentation/theme/colors_app.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // Инициализация зависимостей
+  final WeatherLocalDataSource localDataSource = WeatherLocalDataSource();
+  final SaveWeatherUseCase saveWeatherUseCase =
+      SaveWeatherUseCase(localDataSource);
+  final LoadWeatherUseCase loadWeatherUseCase =
+      LoadWeatherUseCase(localDataSource);
+
+  runApp(MyApp(
+    saveWeatherUseCase: saveWeatherUseCase,
+    loadWeatherUseCase: loadWeatherUseCase,
+  ));
+}
 
 class MyApp extends StatelessWidget {
+  final SaveWeatherUseCase saveWeatherUseCase;
+  final LoadWeatherUseCase loadWeatherUseCase;
+
+  const MyApp({
+    Key? key,
+    required this.saveWeatherUseCase,
+    required this.loadWeatherUseCase,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +39,10 @@ class MyApp extends StatelessWidget {
         primaryColor: AppColors.primary,
         scaffoldBackgroundColor: AppColors.background,
       ),
-      home: const WeatherScreen(),
+      home: WeatherScreen(
+        saveWeatherUseCase: saveWeatherUseCase,
+        loadWeatherUseCase: loadWeatherUseCase,
+      ),
     );
   }
 }
