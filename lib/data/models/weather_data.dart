f113a1f7fe96113b883737098a5ba06ee8ca9b5d
Sergey <chiki73rus@yandex.ru>
2024-10-03 16:50:37 +0400

    Добавил поле icon в WeatherData для хранения кода иконки погоды и изменил метод fromJson для извлечения иконки из ответа API OpenWeatherMap

--- lib/data/models/weather_data.dart
+++ lib/data/models/weather_data.dart
@@ -3,12 +3,14 @@ class WeatherData {
   final int humidity;
   final double windSpeed;
   final double feelsLike;
+  final String icon;
 
   WeatherData({
     required this.temperature,
     required this.humidity,
     required this.windSpeed,
     required this.feelsLike,
+    required this.icon,
   });
 
   factory WeatherData.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,7 @@ class WeatherData {
       humidity: json['main']['humidity'],
       windSpeed: json['wind']['speed'],
       feelsLike: json['main']['feels_like'],
+      icon: json['weather'][0]['icon'],
     );
   }
 }
